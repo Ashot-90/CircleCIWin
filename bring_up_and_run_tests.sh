952f35aa75f4cbfb5544441c7befd90d1d6cd029
Ashot Avagyan <75694354+Ashot-90@users.noreply.github.com>
2020-12-16 11:19:41 +0400

    Update bring_up_and_run_tests.sh

--- bring_up_and_run_tests.sh
+++ bring_up_and_run_tests.sh
@@ -1,6 +1,6 @@
-cd /c/download/ta_assignment/
-../npm run -s start-api &
-../npm start &
+cd /c/download/
+npm run -s start-api &
+npm start &
 sleep 5
 $(npm bin)/cypress run --browser chrome
 $(npm bin)/cypress run --browser firefox
