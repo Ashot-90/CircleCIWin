2a8f8f364be5409b51d7e0aa9d0963585a52e7ac
Ashot Avagyan <75694354+Ashot-90@users.noreply.github.com>
2020-12-16 11:07:56 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 mkdir -p /c/download
-cp -r ta_assignment /c/download/
+cp -r ta_assignment/* /c/download/
 # Install NodeJS and Cypress
 curl "https://nodejs.org/dist/v12.16.1/node-v12.16.1-win-x64.zip" -o /c/download/nodejs.zip
 cd /c/download
 unzip nodejs.zip
 export PATH=$PATH:/c/download/node-v12.16.1-win-x64
-#echo "NodeJS is : $(ls /c/download/node-v12.16.1-win-x64/)"
-#cd /c/download/node-v12.16.1-win-x64/
-#./npm install cypress@4.2.0 --save-dev --global
-cd /c/download/ta_assignment/
 ./npm install
 ./npm audit fix
