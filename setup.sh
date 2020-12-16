ebcffab6360af6597360da09946f3cb544f4b5cf
Ashot Avagyan <75694354+Ashot-90@users.noreply.github.com>
2020-12-16 11:26:52 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 mkdir -p /c/download
 cp -r ta_assignment/* /c/download/
-# Install NodeJS and Cypress
+# Install NodeJS
 curl "https://nodejs.org/dist/v12.16.1/node-v12.16.1-win-x64.zip" -o /c/download/nodejs.zip
 cd /c/download
 unzip nodejs.zip
 export PATH=$PATH:/c/download/node-v12.16.1-win-x64
-echo "Dir content is : $(ls /c/download/node-v12.16.1-win-x64/*/*)"
 npm install
 npm audit fix
