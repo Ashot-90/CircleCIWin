ac01b66b24b11c6f72371ef31c4c82e02a85e2ef
Ashot Avagyan <75694354+Ashot-90@users.noreply.github.com>
2020-12-16 15:32:05 +0400

    Update bring_up_and_run_tests.sh

--- bring_up_and_run_tests.sh
+++ bring_up_and_run_tests.sh
@@ -1,5 +1,9 @@
 cd /c/download
 export PATH=$PATH:/c/download/node-v12.16.1-win-x64
+export PATH=$PATH:/c/Program Files (x86)/Google/Chrome/Application/
+export PATH=$PATH:/c/Program Files/Google/Chrome/Application/
+export PATH=$PATH:/c/Program Files (x86)/Mozilla Firefox/
+export PATH=$PATH:/c/Program Files/Mozilla Firefox/
 npm run -s start-api &
 npm start &
 sleep 5
