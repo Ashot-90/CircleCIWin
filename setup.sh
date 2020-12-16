c629c4afbd67497248b795210e47b9b4e6df09b0
Ashot Avagyan <75694354+Ashot-90@users.noreply.github.com>
2020-12-16 12:28:08 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,9 +7,3 @@ unzip nodejs.zip
 export PATH=$PATH:/c/download/node-v12.16.1-win-x64
 npm install
 npm audit fix
-
-# Install Chrome
-curl -L "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -o /c/download/chrome_installer.exe
-./chrome_installer.exe &
-echo "Waiting to install chrome..."
-sleep 20
