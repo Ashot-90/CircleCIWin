13b2b8a473910fb5285b9da2befa1c92b9d62113
Ashot Avagyan <75694354+Ashot-90@users.noreply.github.com>
2020-12-16 13:30:20 +0400

    Update bring_up_and_run_tests.sh

--- bring_up_and_run_tests.sh
+++ bring_up_and_run_tests.sh
@@ -3,5 +3,9 @@ cd /c/download/
 npm run -s start-api &
 npm start &
 sleep 5
+echo "AA : $(ls /usr/bin/google-chrome)"
+echo "BB : $(ls /opt/google/chrome/chrome)"
+ln -s /usr/bin/google-chrome /opt/google/chrome/chrome
+export PATH=$PATH:/opt/google/chrome/
 $(npm bin)/cypress run --browser chrome
 $(npm bin)/cypress run --browser firefox
