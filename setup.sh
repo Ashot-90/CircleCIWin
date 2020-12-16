42d5381a942a77373271b8797140fd9c89f31903
Ashot Avagyan <75694354+Ashot-90@users.noreply.github.com>
2020-12-16 14:46:26 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 mkdir -p /c/download
 cp -r ta_assignment/* /c/download/
+echo "PATH is : $PATH"
+echo "CHROME is : '/c/Program Files (x86)/Google/Chrome/Application/'"
+echo "FIREFOX is : '/c/Program Files (x86)/Mozilla Firefox/'"
+echo "FIREFOX is : '/c/Program Files/Mozilla Firefox/'"
+echo "CHROME is : '/c/Program Files/Google/Chrome/Application/'"
 # Install NodeJS
 curl "https://nodejs.org/dist/v12.16.1/node-v12.16.1-win-x64.zip" -o /c/download/nodejs.zip
 cd /c/download
