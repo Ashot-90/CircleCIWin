ed4cdfcf043691438f76e33905cf0ec3f9cea4ee
Ashot Avagyan <75694354+Ashot-90@users.noreply.github.com>
2020-12-16 10:38:27 +0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+mkdir -p /c/download
+cp -r ta_assignment /c/download/
+# Install NodeJS and Cypress
+curl "https://nodejs.org/dist/v12.16.1/node-v12.16.1-win-x64.zip" -o /c/download/nodejs.zip
+cd /c/download
+unzip nodejs.zip
+cd /c/download/node-v12.16.1-win-x64/
+./npm install cypress@4.2.0 --save-dev --global
+cd /c/download/ta_assignment/
+../npm install
